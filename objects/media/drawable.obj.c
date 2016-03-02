106ed20f17a2ff0ab9a4a3ef772f4b545dfb47e9
nardinan <andrea@nardinan.it>
2016-03-03 00:25:12 +0100

    A new procedure to keep the same scale has been implemented in the drawable object. Also some fixes in the scale have been implemented.

--- objects/media/drawable.obj.c
+++ objects/media/drawable.obj.c
@@ -87,13 +87,17 @@ d_define_method(drawable, normalize_scale)(struct s_object *self, double referen
 	new_y = (this_y + this_center_y) - (this_center_y * drawable_attributes->zoom);
 	new_x = (new_x + focus_x) - (focus_x * zoom);
 	new_y = (new_y + focus_y) - (focus_y * zoom);
-	new_x = ((new_x * current_w)/reference_w) - offset_x;
-	new_y = ((new_y * current_h)/reference_h) - offset_y;
 	this_zoom = drawable_attributes->zoom * zoom;
-	new_w = ((this_w * current_w)/reference_w) * this_zoom;
-	new_h = ((this_h * current_h)/reference_h) * this_zoom;
-	new_center_x = ((this_center_x * new_w)/this_w);
-	new_center_y = ((this_center_y * new_h)/this_h);
+	new_w = this_w * this_zoom;
+	new_h = this_h * this_zoom;
+	new_center_x = this_center_x * this_zoom;
+	new_center_y = this_center_y * this_zoom;
+	new_x = (new_x * current_w)/reference_w - offset_x;
+	new_y = (new_y * current_h)/reference_h - offset_y;
+	new_w = (new_w * current_w)/reference_w;
+	new_h = (new_h * current_h)/reference_h;
+	new_center_x = (new_center_x * current_w)/reference_w;
+	new_center_y = (new_center_y * current_h)/reference_h;
 	d_call(&(drawable_attributes->point_normalized_destination), m_point_set_x, new_x);
 	d_call(&(drawable_attributes->point_normalized_destination), m_point_set_y, new_y);
 	d_call(&(drawable_attributes->point_normalized_dimension), m_point_set_x, new_w);
@@ -109,6 +113,29 @@ d_define_method(drawable, normalize_scale)(struct s_object *self, double referen
 	/* is the object still visible ? */
 	if ((drawable_attributes->flags&e_drawable_kind_force_visibility) != e_drawable_kind_force_visibility)
 		if ((new_x > current_w) || (new_y > current_h) || (new_x < -new_w) || (new_y < -new_h))
+			result = NULL;
+	return result;
+}
+
+d_define_method(drawable, keep_scale)(struct s_object *self, double current_w, double current_h) {
+	d_using(drawable);
+	double this_x, this_y, this_w, this_h, this_center_x, this_center_y;
+	struct s_object *result = self;
+	d_call(&(drawable_attributes->point_destination), m_point_get, &this_x, &this_y);
+	d_call(&(drawable_attributes->point_dimension), m_point_get, &this_w, &this_h);
+	d_call(&(drawable_attributes->point_center), m_point_get, &this_center_x, &this_center_y);
+	d_call(&(drawable_attributes->point_normalized_destination), m_point_set_point, &(drawable_attributes->point_destination));
+	d_call(&(drawable_attributes->point_normalized_dimension), m_point_set_point, &(drawable_attributes->point_dimension));
+	d_call(&(drawable_attributes->point_normalized_center), m_point_set_point, &(drawable_attributes->point_center));
+	d_call(&(drawable_attributes->square_collision_box), m_square_set_top_left_x, this_x, d_false);
+	d_call(&(drawable_attributes->square_collision_box), m_square_set_top_left_y, this_y, d_false);
+	d_call(&(drawable_attributes->square_collision_box), m_square_set_bottom_right_x, (this_x+this_w));
+	d_call(&(drawable_attributes->square_collision_box), m_square_set_bottom_right_y, (this_x+this_w));
+	d_call(&(drawable_attributes->square_collision_box), m_square_set_angle, drawable_attributes->angle);
+	d_call(&(drawable_attributes->square_collision_box), m_square_set_center, this_center_x, this_center_y);
+	/* is the object still visible ? */
+	if ((drawable_attributes->flags&e_drawable_kind_force_visibility) != e_drawable_kind_force_visibility)
+		if ((this_x > current_w) || (this_y > current_h) || (this_x < -this_w) || (this_y < -this_h))
 			result = NULL;
 	return result;
 }
@@ -222,6 +249,7 @@ d_define_class(drawable) {
 	d_hook_method(drawable, e_flag_public, set_maskA),
 	d_hook_method(drawable, e_flag_public, set_blend),
 	d_hook_method(drawable, e_flag_public, normalize_scale),
+	d_hook_method(drawable, e_flag_public, keep_scale),
 	d_hook_method(drawable, e_flag_public, set_position),
 	d_hook_method(drawable, e_flag_public, get_position),
 	d_hook_method(drawable, e_flag_public, get_scaled_position),
--- objects/media/drawable.obj.h
+++ objects/media/drawable.obj.h
@@ -61,6 +61,7 @@ d_declare_method(drawable, set_maskA)(struct s_object *self, unsigned int alpha)
 d_declare_method(drawable, set_blend)(struct s_object *self, enum e_drawable_blends blend); /* abstract */
 d_declare_method(drawable, normalize_scale)(struct s_object *self, double reference_w, double reference_h, double offset_x, double offset_y,
 		double focus_x, double focus_y, double current_w, double current_h, double zoom);
+d_declare_method(drawable, keep_scale)(struct s_object *self, double current_w, double current_h);
 d_declare_method(drawable, set_position)(struct s_object *self, double x, double y);
 d_declare_method(drawable, get_position)(struct s_object *self, double *x, double *y);
 d_declare_method(drawable, get_scaled_position)(struct s_object *self, double *x, double *y);
