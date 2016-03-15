a15c0b88dd88c028dcb22104aed32b967b468de1
nardinan <andrea@nardinan.it>
2016-03-15 23:45:47 +0100

    New library have been included in the environment header

--- objects/media/environment.obj.h
+++ objects/media/environment.obj.h
@@ -19,6 +19,7 @@
 #define miranda_media_environment_h
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 #include "drawable.obj.h"
