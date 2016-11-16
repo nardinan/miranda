b3a9f14052e687b8e16f676199089a1c001f95f5
nardinan <andrea@nardinan.it>
2016-11-16 23:09:29 +0100

    Distance
    A macro to calculate the square distance between two points has been implemented in the Point class.

--- objects/geometry/point.obj.h
+++ objects/geometry/point.obj.h
@@ -21,6 +21,7 @@
 #include "../mutex.obj.h"
 #include "../../math.local.h"
 #define d_point_precision_default 5
+#define d_point_square_distance(x1,y1,x2,y2) ((((x1)-(x2))*((x1)-(x2)))+(((y1)-(y2))*((y1)-(y2))))
 d_declare_class(point) {
     struct s_attributes head;
     double x, y;
